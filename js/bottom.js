1adddcfc232d4394d1ded5e3f2f164e46254bb9a
Eva Hernández González <vali.admin@gmail.com>
2026-05-02 19:53:19 +0200

    Add files via upload

--- js/bottom.js
+++ js/bottom.js
@@ -1,25 +1,13 @@
 // JavaScript Document
 
-/*console.log( "hola bottom" );*/
-
 $( document ).ready(function() {
-	/*console.log( "document loaded" );*/
-		
-	/*const miTitulo = document.querySelector("h2");
-	miTitulo.textContent = "¡Hola mundo 3!";*/
-		
-		
-$( "#light" ).on( "click", function() {
-	$('#body').attr('data-bs-theme', 'light');
-} );
+	
+	$("#showFilters").click(function(){
+		$("#filters").slideDown();
+	});
+	
+	$("#closeFilters").click(function(){
+		$("#filters").slideUp();
+	});
 
-$( "#dark" ).on( "click", function() {
-	$('#body').attr('data-bs-theme', 'dark');
-} );
-		
-		
-    });
-
-    $( window ).on( "load", function() {
-        /*console.log( "window loaded" );*/
-    });
+});
